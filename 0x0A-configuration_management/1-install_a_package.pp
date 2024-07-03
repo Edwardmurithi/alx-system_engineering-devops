4201f4c9d909903e8bb025d0e0ca2301c708a0a5
Edward murithi <murithiedward330@gmail.com>
2024-07-03 11:59:06 +0300

    install flask from pip3.

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,15 +1,6 @@
-# 1-install_a_package.pp
-node default {
-  # Ensure pip3 is installed
-  package { 'python3-pip':
-    ensure => installed,
-  }
+# Installs puppet-lint
 
-  # Ensure Flask version 2.1.0 is installed via pip3
-  package { 'flask':
-    ensure          => '2.1.0',
-    provider        => 'pip3',
-    require         => Package['python3-pip'],
-  }
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
-
